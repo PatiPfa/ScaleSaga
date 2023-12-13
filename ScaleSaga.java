3a7467acd3cdb9c1be2863f66b778e0e18d7a6b8
Julian Schörg <julian.schoerg@stud.fh-campuswien.ac.at>
2023-12-13 15:07:02 +0100

    sout hinzugefügt

--- ScaleSaga.java
+++ ScaleSaga.java
@@ -2,6 +2,7 @@ public class ScaleSaga {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("ScaleSaga");
+        System.out.println("Ich mag Züge");
     }
 
 }
