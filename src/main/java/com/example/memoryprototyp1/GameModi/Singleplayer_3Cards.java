c4bfa05e0d3999727ae4fe434540896daa41891a
Lorberski <lorenz.rettenwender@stud.fh-campuswien.ac.at>
2024-01-06 10:10:09 +0100

    More Cards in all GameModis all modis can be started

--- src/main/java/com/example/memoryprototyp1/GameModi/Singleplayer_3Cards.java
+++ src/main/java/com/example/memoryprototyp1/GameModi/Singleplayer_3Cards.java
@@ -3,9 +3,10 @@ package com.example.memoryprototyp1.GameModi;
 import com.example.memoryprototyp1.GameModi.BaseGame;
 import javafx.scene.layout.FlowPane;
 
-public class Singleplayer_3Cards extends BaseGame {
+public class Singleplayer_3Cards extends MultiplayerForTwo_3Cards {
     public Singleplayer_3Cards(int flowPaneSize, FlowPane imagesFlowPane) {
         super(flowPaneSize, imagesFlowPane);
     }
+
 
 }
