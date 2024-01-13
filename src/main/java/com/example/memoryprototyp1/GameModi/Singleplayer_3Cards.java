c300eae50f3c3ab74a304ef7e1f5bfc681950a71
Julian Schörg <julian.schoerg@stud.fh-campuswien.ac.at>
2024-01-13 01:37:13 +0100

    Singleplayer_3Cards extends now BaseGame, not Singleplayer_3Cards 2 Cards

--- src/main/java/com/example/memoryprototyp1/GameModi/Singleplayer_3Cards.java
+++ src/main/java/com/example/memoryprototyp1/GameModi/Singleplayer_3Cards.java
@@ -4,7 +4,7 @@ import com.example.memoryprototyp1.GameModi.BaseGame;
 import javafx.scene.control.Alert;
 import javafx.scene.layout.FlowPane;
 
-public class Singleplayer_3Cards extends MultiplayerForTwo_3Cards {
+public class Singleplayer_3Cards extends BaseGame {
     public Singleplayer_3Cards(int flowPaneSize, FlowPane imagesFlowPane) {
         super(flowPaneSize, imagesFlowPane);
     }
