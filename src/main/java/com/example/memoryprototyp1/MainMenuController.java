3045641c68f9c0f56b03d1e606e770c46eddcbf1
Patrick <p.pfaffl@gmx.at>
2024-01-21 20:59:08 +0100

    sound

--- src/main/java/com/example/memoryprototyp1/MainMenuController.java
+++ src/main/java/com/example/memoryprototyp1/MainMenuController.java
@@ -91,18 +91,19 @@ public class MainMenuController {
     private final Image CURSOR = new Image(Objects.requireNonNull(Card.class.getResourceAsStream("images/sword.png")));
 
     /*
-    * Effekt wenn man über button streicht
-    * return: <a target="_blank" href="https://icons8.com/icon/7806/left-2">Left 2</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-    * einstellungen button: <a target="_blank" href="https://icons8.com/icon/2969/einstellungen">Einstellungen</a> Icon von <a target="_blank" href="https://icons8.com">Icons8</a>
-    * */
+     * Effekt wenn man über button streicht
+     * return: <a target="_blank" href="https://icons8.com/icon/7806/left-2">Left 2</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+     * einstellungen button: <a target="_blank" href="https://icons8.com/icon/2969/einstellungen">Einstellungen</a> Icon von <a target="_blank" href="https://icons8.com">Icons8</a>
+     * */
 
     private static String gamemode;
+
     public static String getGamemode() {
         return gamemode;
     }
 
     public static void setSingleplayer(boolean newState) {
-    singleplayer = newState;
+        singleplayer = newState;
     }
 
     public static String getPlayer1name() {
@@ -113,32 +114,32 @@ public class MainMenuController {
         return player2name;
     }
 
-    public void singleplayer(){
+    public void singleplayer() {
         playButtonSound();
         singleplayer = true;
         switchToSubmenu();
     }
 
-    public void multiplayer(){
+    public void multiplayer() {
         playButtonSound();
         switchToSubmenu();
         scoreBoardtwoCards.setVisible(false);
         scoreBoardthreeCards.setVisible(false);
     }
 
-    public void switchToSubmenu(){
+    public void switchToSubmenu() {
         mainMenuAP.setVisible(false);
         SubMenuAP.setVisible(true);
         scoreDisplay();
     }
 
-    public void backButton(){
+    public void backButton() {
         mainMenuAP.setVisible(true);
         SubMenuAP.setVisible(false);
     }
 
-    public Stage switchToGame(ActionEvent event, String mode){
-        try{
+    public Stage switchToGame(ActionEvent event, String mode) {
+        try {
             root = FXMLLoader.load(getClass().getResource(mode));
             stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             scene = new Scene(root);
@@ -146,24 +147,26 @@ public class MainMenuController {
             stage.setScene(scene);
             stage.show();
             return stage;
-        }catch (Exception e){
+        } catch (Exception e) {
             writeInLog(e, gamemode);
         }
         return null;
     }
-    public void switchToSetting(ActionEvent event){
-        try{
+
+    public void switchToSetting(ActionEvent event) {
+        try {
             root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Setting.fxml")));
             stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
-        }catch (Exception e){
+        } catch (Exception e) {
             writeInLog(e, "Setting");
         }
 
     }
-    public void switchToMenu(ActionEvent event){
+
+    public void switchToMenu(ActionEvent event) {
         try {
             root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainMenu.fxml")));
             stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -174,48 +177,52 @@ public class MainMenuController {
             writeInLog(e, "MainMenu");
         }
     }
-    public void twoCards(ActionEvent event){
-        if(singleplayer){
+
+    public void twoCards(ActionEvent event) {
+        if (singleplayer) {
             gamemode = "Singleplayer2Cards";
             playButtonSound();
-            try{
+            try {
                 Stage stage = switchToGame(event, "Singleplayer_2Cards.fxml");
                 stage.setTitle("Singleplayer 2 Cards");
                 stage.show();
-            }catch (Exception e){
+            } catch (Exception e) {
                 writeInLog(e, "Singleplayer 2 Cards");
             }
 
-        }else{
+        } else {
             gamemode = "Multiplayer2Cards";
             playButtonSound();
             switchMainToNames();
         }
     }
-    public void threeCards(ActionEvent event){
-        if(singleplayer){
+
+    public void threeCards(ActionEvent event) {
+        if (singleplayer) {
             gamemode = "Singleplayer3Cards";
             playButtonSound();
-            try{
+            try {
                 Stage stage = switchToGame(event, "Singleplayer_3Cards.fxml");
                 stage.setTitle("Singleplayer 3 Cards");
                 stage.show();
-            }catch (Exception e){
+            } catch (Exception e) {
                 writeInLog(e, "Singleplayer 3 Cards");
             }
 
-        }else{
+        } else {
             gamemode = "Multiplayer3Cards";
             playButtonSound();
             switchMainToNames();
         }
     }
-    public void switchMainToNames(){
+
+    public void switchMainToNames() {
         SubMenuAP.setVisible(!SubMenuAP.isVisible());
         nameInputAP.setVisible(!nameInputAP.isVisible());
         label_errormessage.setText(" ");
     }
-    public void closeGame(){
+
+    public void closeGame() {
         Alert alert = new Alert(Alert.AlertType.WARNING);
         alert.setTitle("Quit game");
         alert.setHeaderText("Do you really want to quit?");
@@ -231,18 +238,18 @@ public class MainMenuController {
         }
     }
 
-    public void submitNames(ActionEvent event){
+    public void submitNames(ActionEvent event) {
         player1name = tf_player1.getText();
         player2name = tf_player2.getText();
 
         //TODO:  schauen ob die Namenslängen passen
-        if(player1name.length() < 3 || player2name.length() < 3){
+        if (player1name.length() < 3 || player2name.length() < 3) {
             label_errormessage.setText("All names must contain at least 3 characters!");
             return;
-        }else if(player1name.length() > 10 || player2name.length() > 10) {
+        } else if (player1name.length() > 10 || player2name.length() > 10) {
             label_errormessage.setText("The name may consist of a maximum of 10 characters!");
             return;
-        } else if(player1name.equals(player2name)){
+        } else if (player1name.equals(player2name)) {
             label_errormessage.setText("The names should not be the same!");
             return;
         }
@@ -252,45 +259,47 @@ public class MainMenuController {
 
         //TODO: ev überlegen das mit dem szenen laden und den buttons noch anders zu machen..
         //vorallem weil das jz hier nicht so schön ausschaut
-        if(gamemode.equals("Multiplayer2Cards")){
+        if (gamemode.equals("Multiplayer2Cards")) {
 
-            try{
+            try {
                 Stage stage = switchToGame(event, "MultiplayerForTwo_2Cards.fxml");
                 stage.setTitle("Multiplayer 2 Cards");
                 stage.show();
-            }catch (Exception e){
+            } catch (Exception e) {
                 writeInLog(e, "Multiplayer 2 Cards");
             }
 
-        }else{
+        } else {
             playButtonSound();
-            try{
+            try {
                 Stage stage = switchToGame(event, "MultiplayerForTwo_3Cards.fxml");
                 stage.setTitle("Multiplayer 3 Cards");
                 stage.show();
-            }catch (Exception e){
+            } catch (Exception e) {
                 writeInLog(e, "Multiplayer 3 Cards");
             }
 
         }
 
     }
+
     //Speichert die Highscore namen in die Labels
-    public void scoreDisplay(){
+    public void scoreDisplay() {
 
-        setScoreLabel(one1, 0);
-        setScoreLabel(one2, 1);
-        setScoreLabel(one3, 2);
-        setScoreLabel(one4, 3);
-        setScoreLabel(one5,4);
+        setScoreLabel(one1, 0, txtFileTwoCards);
+        setScoreLabel(one2, 1, txtFileTwoCards);
+        setScoreLabel(one3, 2, txtFileTwoCards);
+        setScoreLabel(one4, 3, txtFileTwoCards);
+        setScoreLabel(one5, 4, txtFileTwoCards);
 
-        setScoreLabel1(two1, 0);
-        setScoreLabel1(two2, 1);
-        setScoreLabel1(two3, 2);
-        setScoreLabel1(two4, 3);
-        setScoreLabel1(two5,4);
+        setScoreLabel(two1, 0, txtFileThreeCards);
+        setScoreLabel(two2, 1, txtFileThreeCards);
+        setScoreLabel(two3, 2, txtFileThreeCards);
+        setScoreLabel(two4, 3, txtFileThreeCards);
+        setScoreLabel(two5, 4, txtFileThreeCards);
     }
-    public void returnFromNames(){
+
+    public void returnFromNames() {
         playButtonSound();
         tf_player1.setText("Player 1");
         tf_player2.setText("Player 2");
@@ -305,26 +314,29 @@ public class MainMenuController {
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
             String timestamp = dateFormat.format(new Date());
 
-            writer.println("["+ timestamp + "] Fehler beim Wechseln zur " + Fehlerseite + "-Seite:");
+            writer.println("[" + timestamp + "] Fehler beim Wechseln zur " + Fehlerseite + "-Seite:");
             e.printStackTrace(writer);
             writer.println();
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
     }
+
     @FXML
     private void toggleMute(ActionEvent event) {
         Music.MusicPlayer.toggleMute();
     }
+
     @FXML
     private void handleVolumeChange(MouseEvent event) {
         double volume = sliderVolume.getValue();
         Music.MusicPlayer.setBackgroundMusicVolume(volume);
     }
-    private void setScoreLabel(Label l, int pos) {
-        Score[] scores = readHighscore();
 
-        scores[pos] = readHighscore()[pos];
+    private void setScoreLabel(Label l, int pos, String path) {
+        Score[] scores = readHighscore(path);
+
+        scores[pos] = readHighscore(path)[pos];
         if (scores[pos] != null && scores[pos].getScoreSec() < 10) {
             l.setText(scores[pos].getScoreMin() + ":0" + scores[pos].getScoreSec() + " | " + scores[pos].getPlayerName());
         } else if (scores[pos] != null) {
@@ -332,45 +344,21 @@ public class MainMenuController {
         }
 
     }
- //private void setScoreLabel1(Label l, int pos) {
- //    Score[] scores = readHighscore();
- //
- //    scores[pos] = readHighscore1()[pos];
- //    if (scores[pos] != null && scores[pos].getScoreSec() < 10) {
- //        l.setText(scores[pos].getScoreMin() + ":0" + scores[pos].getScoreSec() + " | " + scores[pos].getPlayerName());
- //    } else if (scores[pos] != null) {
- //        l.setText(scores[pos].getScoreMin() + ":" + scores[pos].getScoreSec() + " | " + scores[pos].getPlayerName());
- //    }
- //
- //}
 
-    public static Score[] readHighscore(String path){
+    public static Score[] readHighscore(String path) {
         Score[] out = new Score[5];
 
 
         try {
-            FileInputStream fileIn = new FileInputStream(txtFileTwoCards);
+            FileInputStream fileIn = new FileInputStream(path);
             ObjectInputStream in = new ObjectInputStream(fileIn);
             out = (Score[]) in.readObject();
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Array is empty why");
         }
 
         return out;
     }
-    public static Score[] readHighscore1(){
-        Score[] out = new Score[5];
-
-
-        try {
-            FileInputStream fileIn = new FileInputStream(txtFileThreeCards);
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            out = (Score[]) in.readObject();
-        } catch (Exception e){
-            System.out.println("Array is empty why");
-        }
-
-        return out;
-    }
-
 }
+
+
